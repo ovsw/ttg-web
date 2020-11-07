8600d7df1cd1baff01f1bf62824b5af4b52e5037
Ovi Savescu <ovi@ovswebsites.com>
2020-11-07 08:14:58 +0200

    added gulp

--- .eleventy.js
+++ .eleventy.js
@@ -26,6 +26,9 @@ module.exports = config => {
   })
   // end markdown 
 
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
