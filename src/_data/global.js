0c6392ec93ecb464bc9ada9ee070094a9c607a76
Ovi Savescu <ovi@ovswebsites.com>
2020-11-09 20:50:07 +0200

    get language root from URL

--- src/_data/global.js
+++ src/_data/global.js
@@ -4,5 +4,10 @@ module.exports = {
       return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
     return `${segment()}-${segment()}-${segment()}`;
+  },
+
+  getCurrentLanguage(pageUrl) {
+    const language =  pageUrl == "/" ? "en" : pageUrl.substring(1, 3);
+    return language;
   }
 };
